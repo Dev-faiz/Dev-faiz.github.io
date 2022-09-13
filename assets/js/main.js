a97fb6474e9f466c16ceb35a8f08d05c89f3ab55
Dev-faiz <faizkfz@gmail.com>
2022-09-13 18:39:01 +0530

    roadMap

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,40 @@ function linkAction(){
 navLink.forEach(element => {
     element.addEventListener('click',linkAction)
 });
+
+// -------------------------- ACCORDION 
+const skillsContent = document.getElementsByClassName("skill_content"),
+        skillsHeader = document.querySelectorAll('.skill_header')
+
+function toggleSkill(){
+    let itemClass = this.parentNode.className
+    for(let i = 0 ; i < skillsContent.length ; i++){
+        skillsContent[i].className = "skill_content skill_close"
+    }
+    if(itemClass == "skill_content skill_close"){
+        this.parentNode.className = "skill_content skill_open"
+    }
+}
+
+skillsHeader.forEach((e)=>{
+    e.addEventListener('click',toggleSkill)
+})
+
+// qualification tabs
+
+// const tabs = document.querySelectorAll('[data-target]'),
+//         tabContents = document.querySelectorAll('[data-content]')
+
+//     tabs.forEach(tab=>{
+//         const target = document.querySelector(tab.dataset.target)
+//         tabContents.forEach(tabContent =>{
+//             tabContent.classList.remove('qualification_active')
+//         })
+//         target.classList.add('qualification_active')
+//         tab.forEach(tab=>{
+//             tab.classList.remove('qualification_active')
+//         })
+//         tab.classList.add('qualification_active')
+//     })
+
+// services
